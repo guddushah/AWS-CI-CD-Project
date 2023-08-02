9c8bb1066db7a1bd66520b2d2f19b0c94411e90e
guddu shah <40028193+guddushah@users.noreply.github.com>
2023-08-03 00:46:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,7 @@ def predict_datapoint():
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=float(request.form.get('reading_score')),
-            writing_score=float(request.form.get('writing_score')),
-
+            writing_score=float(request.form.get('writing_score'))
         )
 
         pred_df=data.get_data_as_data_frame() # returned dataframe from pred_pipeline
